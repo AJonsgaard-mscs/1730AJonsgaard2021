1f111cc59e33bac53f4dd511d0f51dd761156561
AJonsgaard-mscs <alexanndriadue95@gmail.com>
2021-09-11 12:45:19 -0500

    AverageTestScore1E complete

---
+++ AJonsgaard1E1/AverageTestScore1E1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AJonsgaard1E1
+{
+    public partial class AverageTestScore1E1 : Form
+    {
+        public AverageTestScore1E1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            txtAverage.Text = ((Convert.ToDecimal(txtTest1.Text) +
+                Convert.ToDecimal(txtTest2.Text) +
+                Convert.ToDecimal(txtTest3.Text)) / 3).ToString("0.00");
+            btnClear.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtTest1.Text = ("0");
+            txtTest2.Text = ("0");
+            txtTest3.Text = ("0");
+            txtAverage.Text = ("");
+            txtTest1.Focus();
+        }
+    }
+}
